a743fb4466d1e5ccf37ba3d4af8cbfbfbd77e0bf
Mattia Dal Ben <matthewdibi@gmail.com>
2022-02-24 17:07:41 +0100

    MODEL: Switched to MinMax scaling

--- models/preprocessor/1/model.py
+++ models/preprocessor/1/model.py
@@ -88,11 +88,11 @@ class TritonPythonModel:
             out_0 = np.array([magnet_x, magnet_z, magnet_y, acc_y, acc_x, acc_z, pressure, temp_press, temp_hum, humidity, gyro_x, gyro_y, gyro_z]).transpose()
 
             #                MAGNET_X   MAGNET_Z   MAGNET_Y     ACC_Y     ACC_X     ACC_Z    PRESSURE   TEMP_PRESS   TEMP_HUM   HUMIDITY    GYRO_X    GYRO_Y    GYRO_Z
-            mean = np.array([-6.662364, 15.270208, -27.288259, 0.030234, 0.011407, 0.992252, 998.028151, 35.338573, 36.764190, 21.211922, -0.026414, 0.008353, 0.005063])
-            std  = np.array([ 5.250766,  0.853466,   1.319709, 0.028152, 0.089823, 0.024537,   0.035404,  0.376381,  0.336227,  0.613346,  1.096187, 0.498696, 0.146555])
+            min = np.array([-1.8199598e+01, 1.3105305e+01, -3.1945429e+01, -1.0663300e-01, -3.1721205e-01, 8.5927510e-01, 9.9792896e+02, 3.4666668e+01, 3.6153427e+01, 2.0175404e+01, -2.1469840e+00, -1.3511374e+00, -5.0122184e-01])
+            max = np.array([ 3.7974422e+00, 1.7207603e+01, -2.4966690e+01, 1.1758627e-01, 2.4764843e-01, 1.1285601e+00, 9.9812500e+02, 3.6097916e+01, 3.7271065e+01, 2.3081772e+01, 1.8424674e+00, 7.2500044e-01, 4.4410592e-01])
 
-            # Standard scaling
-            out_0_scaled = (out_0 - mean)/std
+            # MinMax scaling
+            out_0_scaled = (out_0 - min)/(max - min)
 
             # Create output tensors. You need pb_utils.Tensor
             # objects to create pb_utils.InferenceResponse.
